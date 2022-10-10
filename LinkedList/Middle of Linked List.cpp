ac675350005c49ed0fc706c5f47cb0b99dd050b0
Vaishnavi Gupta <113535692+Vaishnavi2445@users.noreply.github.com>
2022-10-10 23:37:24 +0530

    Create Middle of Linked List.cpp

---
+++ LinkedList/Middle of Linked List.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL){
+            return head;
+        }
+        ListNode *slow=head, *fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
